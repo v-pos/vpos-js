d6aa7e18210be78fb66f3f9fc688db550b158f9c
Sergio Maziano <sergio@nextbss.co.ao>
2020-11-06 10:25:52 +0100

    adds the support to create uuid

--- vpos.js
+++ vpos.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const uuid = require('uuid');
 const url = process.env.VPOS_PRODILE == "PRD" ? "https://api.vpos.ao" : "https://sandbox.vpos.ao";
 
 class Vpos {
@@ -14,17 +15,10 @@ class Vpos {
         return config;
     }
 
-    #randomGuid() {
-        S4() {  
-            return (((1+Math.random())*0x10000)|0).toString(16).substring(1);  
-        }  
-        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();  
-    }
-
     getTransaction(transactionId) {
         axios.get(url + '/api/v1/transactions/' + transactionId, this.#requestHeaders())
         .then(function (response) {
-            console.log(response.data);
+            console.log({status: response.status, message: response.statusText, data: response.data});
         })
         .catch(function (error) {
             console.log(error);
@@ -34,7 +28,7 @@ class Vpos {
     getTransactions() {
         axios.get(url + '/api/v1/transactions', this.#requestHeaders())
         .then(function (response) {
-            console.log(response.data);
+            console.log(response);
         })
         .catch(function (error) {
             console.log(error);
@@ -44,8 +38,8 @@ class Vpos {
     newPaymentTransaction(amount, customer, posId) {
         body = {amount: amount, mobile: customer, pos_id: posId, callback: callback_url}
 
-        var header = this.#requestHeader();
-        header.headers['Idempontency'] = this.#randomGuid(); 
+        var header = this.#requestHeaders();
+        header.headers['Idempontency'] = uuid(); 
 
         axios.post(url + '/api/v1/transactions', body, header)
         .then(function (response) {
@@ -55,16 +49,8 @@ class Vpos {
             console.log(error);
         });
     }
-
-    showHeader() {
-        var header = this.#requestHeaders();
-        header.headers['Idempontency'] = '3242374324t7328gr823';
-        console.log(header);
-    }
-
-
 }
 
 let transaction = new Vpos();
-//transaction.getTransaction("1jYQryG3Qo4nzaOKgJxzWDs25Ht");
-transaction.showHeader();
+transaction.getTransaction("1jYQryG3Qo4nzaOKgJxzWDs25Ht");
+//transaction.getTransactions();
