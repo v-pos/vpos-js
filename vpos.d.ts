6a1ed8360d8811250f53bbea524c0732982a2b3d
Pedro Renan <pedrorenan@gmail.com>
2022-05-18 01:19:57 +0100

    changing module to class

--- vpos.d.ts
+++ vpos.d.ts
@@ -1,15 +1,22 @@
-declare namespace Vpos {
-  interface newPaymentSucces {
-    status_code: number;
-    message: string;
-    location: string;
-  }
-  interface newPaymentError {
-    status_code: number;
-    message: string;
-    details: string;
-  }
-  function newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError 
+export interface newPaymentSucces {
+  status_code: number;
+  message: string;
+  location: string;
+}
+interface newPaymentError {
+  status_code: number;
+  message: string;
+  details: string;
 }
 
-export default Vpos
+export interface vPosConfig {
+  token: string;
+  posId: number;
+  supervisorCard: string;
+  paymentCallbackUrl: string;
+  refundCallbackUrl: strin;
+}
+export class Vpos {
+  constructor(config: vPosConfig);
+  newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError 
+}
