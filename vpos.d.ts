1481ce3a2bcb447c430b8d9541f1ff448c2ccda1
Pedro Renan <pedrorenan@gmail.com>
2022-05-18 02:56:46 +0100

    first type definitions working

--- vpos.d.ts
+++ vpos.d.ts
@@ -1,28 +1,28 @@
-interface newPaymentSucces {
+export interface newPaymentSucces {
   status_code: number;
   message: string;
   location: string;
 }
-interface newPaymentError {
+export interface newPaymentError {
   status_code: number;
   message: string;
   details: string;
 }
 
-class Vpos {
+export class Vpos {
   token: string;
   posId: number;
   supervisorCard: string;
   paymentCallbackUrl: string;
   refundCallbackUrl: string;
-  constructor(token?: string, posId?: number, supervisorCard?: string, paymentCallbackUrl?: string, refundCallbackUrl?: string){
+  constructor(token?: string, posId?: number, supervisorCard?: string, paymentCallbackUrl?: string, refundCallbackUrl?: string) {
     this.token = token;
     this.posId = posId;
     this.supervisorCard = supervisorCard;
     this.paymentCallbackUrl = paymentCallbackUrl;
     this.refundCallbackUrl = refundCallbackUrl;
   }
-  newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError 
+  newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError
 }
 
 export default Vpos
