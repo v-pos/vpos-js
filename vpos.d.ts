f3f0d27794c3ddd303ee85ef095f6dca948b7caa
Pedro Renan <pedrorenan@gmail.com>
2022-05-18 01:06:22 +0100

    add export default

--- vpos.d.ts
+++ vpos.d.ts
@@ -1,13 +1,15 @@
 declare namespace Vpos {
-    interface newPaymentSucces {
-        status_code: number;
-        message: string;
-        location: string;
-    }
-    interface newPaymentError {
-        status_code: number;
-        message: string;
-        details: string;
-    }
-    function newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError 
+  interface newPaymentSucces {
+    status_code: number;
+    message: string;
+    location: string;
   }
+  interface newPaymentError {
+    status_code: number;
+    message: string;
+    details: string;
+  }
+  function newPayment(amount: string, customer: string): newPaymentSucces | newPaymentError 
+}
+
+export default Vpos
