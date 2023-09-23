6d30d151974a1549b0e0cc4641fbb22caebbd95d
Mário Varela <mariovarela99@outlook.com>
2023-09-23 13:25:02 +0100

    refactor: cange test vars, all to const

--- test/vpos.test.js
+++ test/vpos.test.js
@@ -1,33 +1,33 @@
 const assert = require('assert');
 const Vpos = require('../vpos.js');
 
-let merchant = new Vpos({});
-let refundId = "";
-let refundTransaction = "";
+const merchant = new Vpos({});
+const refundId = "";
+const refundTransaction = "";
 
 describe('vPOS', () => {
   describe('Payments', () => {
     describe('Positives', () => {
       it('should create a new payment request transaction', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
+        const response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
         assert.strictEqual(response.status_code, 202);
       });
     });
 
     describe('Negatives', () => {
       it('should not create a new payment request transaction if token is invalid', async () => {
-        let merchantPayment = new Vpos({token: 'sdyfisdhfsdiut'})
-        let response = await merchantPayment.newPayment({amount: '123.45', customer: '925888553'});
+        const merchantPayment = new Vpos({token: 'sdyfisdhfsdiut'})
+        const response = await merchantPayment.newPayment({amount: '123.45', customer: '925888553'});
         assert.strictEqual(response.status_code, 401);
       });
 
       it('should not create a new payment request transaction if customer format is invalid', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '92588855'});
+        const response = await merchant.newPayment({amount: '123.45', customer: '92588855'});
         assert.strictEqual(response.status_code, 400);
       });
 
       it('should not create a new payment request transaction if amount format is invalid', async () => {
-        let response = await merchant.newPayment({amount: '123.45.67', customer: '925888553'});
+        const response = await merchant.newPayment({amount: '123.45.67', customer: '925888553'});
         assert.strictEqual(response.status_code, 400);
       });
     });
@@ -36,8 +36,8 @@ describe('vPOS', () => {
   describe('Refunds', () => {
     describe('Positives', () => {
       it('should create a new refund request transaction', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '925888553'})
-        let paymentId = await merchant.requestId({response: response});
+        const response = await merchant.newPayment({amount: '123.45', customer: '925888553'})
+        const paymentId = await merchant.requestId({response: response});
 
         response = await merchant.newRefund({parentTransactionId: paymentId});
         assert.strictEqual(response.status_code, 202);
@@ -46,19 +46,19 @@ describe('vPOS', () => {
 
     describe('Negatives', () => {
       it('should not create a new refund request transaction if token is invalid', async () => {
-        let merchantRefund = new Vpos({token: 'jkshfisdufgsd'})
-        let response = await merchantRefund.newRefund({parentTransactionId: '1jYQryG3Qo4nzaOKgJxzWDs25Hv'});
+        const merchantRefund = new Vpos({token: 'jkshfisdufgsd'})
+        const response = await merchantRefund.newRefund({parentTransactionId: '1jYQryG3Qo4nzaOKgJxzWDs25Hv'});
         assert.strictEqual(response.status_code, 401);
       });
 
       it('should not create a new refund request transaction if parent_transaction_id is not present', async () => {
-        let response = await merchant.newRefund({});
+        const response = await merchant.newRefund({});
         assert.strictEqual(response.status_code, 400);
       });
 
       it('should not create a new refund request transaction if supervisor_card is invalid', async () => {
-        let merchantRefund = new Vpos({supervisorCard: '12345678910111213'});
-        let response = await merchantRefund.newRefund({parentTransactionId: '1jYQryG3Qo4nzaOKgJxzWDs25Hv'});
+        const merchantRefund = new Vpos({supervisorCard: '12345678910111213'});
+        const response = await merchantRefund.newRefund({parentTransactionId: '1jYQryG3Qo4nzaOKgJxzWDs25Hv'});
         assert.strictEqual(response.status_code, 400);
       });
     });
@@ -67,8 +67,8 @@ describe('vPOS', () => {
   describe('Transactions', () => {
     describe('Positives', () => {
       it('should get a single transaction', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '925888553'})
-        let paymentId = await merchant.requestId({response: response});
+        const response = await merchant.newPayment({amount: '123.45', customer: '925888553'})
+        const paymentId = await merchant.requestId({response: response});
 
         response = await merchant.getTransaction({transactionId: paymentId});
         assert.strictEqual(response.status_code, 200);
@@ -77,13 +77,13 @@ describe('vPOS', () => {
 
     describe('Negatives', () => {
       it('should not get a non existent single transaction', async () => {
-        let response = await merchant.getTransaction({transactionId: '1jYQryG3Qo4nzaOKgJxzWDs25H'});
+        const response = await merchant.getTransaction({transactionId: '1jYQryG3Qo4nzaOKgJxzWDs25H'});
         assert.strictEqual(response.status_code, 404);
       });
 
       it('should not get a single transaction if token is invalid', async () => {
-        let merchantTransaction = new Vpos({token: 'kjsdfhsdiufs'})
-        let response = await merchantTransaction.getTransaction({transactionId: '1jYQryG3Qo4nzaOKgJxzWDs25H'});
+        const merchantTransaction = new Vpos({token: 'kjsdfhsdiufs'})
+        const response = await merchantTransaction.getTransaction({transactionId: '1jYQryG3Qo4nzaOKgJxzWDs25H'});
         assert.strictEqual(response.status_code, 401);
       });
     });
@@ -92,24 +92,24 @@ describe('vPOS', () => {
   describe('Requests', () => {
     describe('Positives', () => {
       it('should get a running single request status', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
-        let paymentRequest = await merchant.getRequest({response: response});
+        const response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
+        const paymentRequest = await merchant.getRequest({response: response});
 
         assert.strictEqual(paymentRequest.status_code, 200);
       });
 
-      it('should get a completed single request status', async () => {
-        let response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
+      it('should get a compconsted single request status', async () => {
+        const response = await merchant.newPayment({amount: '123.45', customer: '900000000'});
         assert.strictEqual(response.status_code, 202);
       });
     });
 
     describe('Negatives', () => {
       it('should not get a single request status if token is invalid', async () => {
-        let secondMerchantRequest = new Vpos({token: 'jhfsdfutsdufgs'})
-        let response = await merchant.newPayment({amount: '123.45', customer: '925888553'});
+        const secondMerchantRequest = new Vpos({token: 'jhfsdfutsdufgs'})
+        const response = await merchant.newPayment({amount: '123.45', customer: '925888553'});
 
-        let secondResponse = await secondMerchantRequest.getRequest({response: response})
+        const secondResponse = await secondMerchantRequest.getRequest({response: response})
         assert.strictEqual(secondResponse.status_code, 401);
       });
     });
